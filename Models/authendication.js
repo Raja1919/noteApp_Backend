382b7ace721c8a3c4d3ad5d98295ec31727254f8
Raja1919 <rajaaart51@gmail.com>
2023-12-22 00:35:56 +0530

    final commikt

--- Models/authendication.js
+++ Models/authendication.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const authSchema = new mongoose.Schema({
-  Name: { type: String, required: true },
-  Email: { type: String, required: true },
-  Password: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const Auth = mongoose.model('Auth', authSchema);
--- Routes/authendication.js
+++ Routes/authendication.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const generateToken = require("../genToken");
 const router = express.Router();
-const verifyToken =require('../middleware')
+const verifyToken = require("../middleware");
 
 router.get("/users", async (req, res) => {
   try {
@@ -22,15 +22,22 @@ router.get("/users", async (req, res) => {
 
 router.post("/signup", async (req, res) => {
   try {
-    const { Name, Email, Password } = req.body;
-    const existingUser = await model.findOne({ Email });
+    const { name, email, password } = req.body;
 
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide all required fields" });
+    }
+
+    const existingUser = await model.findOne({ email });
     if (existingUser) {
       return res.status(409).json({ message: "User already exists" });
     }
 
-    const hashedPassword = await bcrypt.hash(Password, 10);
-    const newUser = new model({ Name, Email, Password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new model({ name, email, password: hashedPassword });
     await newUser.save();
 
     return res.status(201).json({ message: "User created successfully" });
@@ -42,14 +49,14 @@ router.post("/signup", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const { Email, Password } = req.body;
-    const user = await model.findOne({ Email });
+    const { email, password } = req.body;
+    const user = await model.findOne({ email });
 
     if (!user) {
       return res.status(404).json({ message: "Incorrect email or password" });
     }
 
-    const passwordValidate = await bcrypt.compare(Password, user.Password);
+    const passwordValidate = await bcrypt.compare(password, user.password);
 
     if (!passwordValidate) {
       return res.status(404).json({ message: "Incorrect email or password" });
@@ -65,9 +72,9 @@ router.post("/login", async (req, res) => {
 
 router.get("/verify", verifyToken, async (req, res) => {
   try {
-      res.send(true);
+    res.send(true);
   } catch (error) {
-      return res.status(500).json({ msg: error.message });
+    return res.status(500).json({ msg: error.message });
   }
 });
 
