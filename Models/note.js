c062fe08c06880327708afbca5565363a5801705
Raja1919 <rajaaart51@gmail.com>
2023-12-21 23:07:28 +0530

    final commit

--- Models/note.js
+++ Models/note.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-});
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model("Note", noteSchema);
 
 module.exports = Note;
--- Routes/note.js
+++ Routes/note.js
@@ -1,77 +1,95 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Note = require('../Models/note');
+const Note = require("../Models/note");
 
 // Get all notes
-router.get('/get', async (req, res) => {
+router.get("/get", async (req, res) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find({ user_id: req.user.id });
     res.json(notes);
   } catch (error) {
-    console.error('Error reading notes:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error reading notes:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // Add a new note
-router.post('/post', async (req, res) => {
+router.post("/post", async (req, res) => {
   try {
-    const { title, content } = req.body;
+    const { title, content, date } = req.body;
 
-    if (!title || !content) {
-      return res.status(400).send('Title and content are required');
+    if (!title || !content || date) {
+      return res.status(400).send("Title and content are required");
     }
 
-    const newNote = new Note({ title, content });
+    const newNote = new Note({ title, content, date });
     await newNote.save();
-
-    res.json(newNote);
+    res.json({ msg: "Created a Note" });
   } catch (error) {
-    console.error('Error adding note:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error adding note:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // Update a note
-router.put('/update/:id', async (req, res) => {
+router.put("/update/:id", async (req, res) => {
   try {
-    const { title, content } = req.body;
+    const { title, content, date } = req.body;
 
-    if (!title || !content) {
-      return res.status(400).send('Title and content are required');
+    if (!title || !content || date) {
+      return res.status(400).send("Title and content are required");
     }
 
     const updatedNote = await Note.findByIdAndUpdate(
       req.params.id,
-      { title, content },
+      { title, content, date },
       { new: true }
     );
 
     if (!updatedNote) {
-      return res.status(404).send('Note not found');
+      return res.status(404).send("Note not found");
     }
 
-    res.json(updatedNote);
+    res.json({ msg: "Updated a Note" });
   } catch (error) {
-    console.error('Error updating note:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error updating note:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // Delete a note
-router.delete('/delete/:id', async (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
   try {
     const deletedNote = await Note.findByIdAndDelete(req.params.id);
 
     if (!deletedNote) {
-      return res.status(404).send('Note not found');
+      return res.status(404).send("Note not found");
     }
 
-    res.json(deletedNote);
+    res.json({ msg: "Deleted a Note" });
   } catch (error) {
-    console.error('Error deleting note:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error deleting note:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
+
+// Get a specific note by ID
+router.get("/get/:id", async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({ msg: "Note not found" });
+    }
+
+    res.json(note);
+  } catch (error) {
+    console.error("Error reading note:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
+
 
 module.exports = router;
